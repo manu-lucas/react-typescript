dacc6a6b3e7838d7636b7cf5e94395b44631f6ae
manu-lucas <lmechegaray@wecom.global>
2024-03-08 15:31:11 -0300

    delete styles sass

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import '../../styles/landing.css'
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,34 +15,11 @@ const Home: React.FC = () => {
 <Aside/>
 
 
-      <h1 className="p-2 text-2xl  m-1" >This is my React app with Tailwind CSS!</h1>
-
-      <div className="flex gap-3 items-center">
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-      <Avatar name="Junior" />
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-      <Avatar name="Jane" />
-      <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-      <Avatar name="Joe" />
-    </div>
+      
    
 
 
 
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          width={270}
-        />
-      </CardBody>
-    </Card>
 
 
 
