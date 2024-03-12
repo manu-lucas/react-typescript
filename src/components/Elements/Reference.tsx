ef109fe71e3106a214f6fe467d3aa7f2e5d5bf34
agugomv <agugomv@gmail.com>
2024-03-12 13:37:05 -0300

    ajustes estéticos nav

--- src/components/Elements/Reference.tsx
+++ src/components/Elements/Reference.tsx
@@ -7,9 +7,8 @@ interface ReferenceProps {
 
 const Reference: React.FC<ReferenceProps> = ({ title, icons }) => {
   return (
-    <div>
-      {icons} {/* Usado directamente, sin <img> */}
-      <h2>{title}</h2>
+    <div className="m-[20px] w-[195px] h-10 bg-white rounded-tr-[15px] rounded-bl-[25px] rounded-br-[15px] pa">
+      <h2 className="w-32 h-3 text-teal-700 text-sm font-bold font-['Poppins']">{title}{icons}</h2>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'verdeFondo': '#2BD99A',
+        'verdePie':'#006F76'
+      },
+
+    },
   },
   darkMode: "class",
   plugins: [nextui()]
