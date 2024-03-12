2635f9aa4e88aaa3885bd5235e6ec91128c490d9
lucaskulisz <lkulisz@wecom.global>
2024-03-12 12:31:11 -0300

    flag cellphone

--- src/components/Elements/Cards/CardClienteContacto.tsx
+++ src/components/Elements/Cards/CardClienteContacto.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Card, CardHeader, Accordion, AccordionItem } from "@nextui-org/react";
+import { PhoneInput } from 'react-international-phone';
+import 'react-international-phone/style.css';
+
 
 
 interface PropsDatosGenerales {
@@ -11,7 +14,7 @@ interface PropsDatosGenerales {
   ciudad?: string;
   nombre?: string;
   email?: string;
-  celular?: string;
+  /* celular?: string; */
   telefono?: string;
   vendedor?: string;
 }
@@ -21,7 +24,8 @@ const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
 
-const CardClienteContacto: React.FC<PropsDatosGenerales> = ({ razon: razonInicial = "", rut: rutInicial = "", giro: giroInicial = "", direccion: direccionInicial = "", comuna: comunaInicial = "", ciudad: ciudadInicial = "", nombre: nombreInicial = "", email: emailInicial = "", celular: celularInicial = "", telefono: telefonoInicial = "", vendedor: vendedorInicial = "" }) => {
+const CardClienteContacto: React.FC<PropsDatosGenerales> = ({ razon: razonInicial = "", rut: rutInicial = "", giro: giroInicial = "", direccion: direccionInicial = "", comuna: comunaInicial = "", ciudad: ciudadInicial = "", nombre: nombreInicial = "", email: emailInicial = "", telefono: telefonoInicial = "", vendedor: vendedorInicial = "" }) => {
+  
   const [razon, setRazon] = useState<string>(razonInicial);
   const [rut, setRut] = useState<string>(rutInicial);
   const [giro, setGiro] = useState<string>(giroInicial);
@@ -30,7 +34,7 @@ const CardClienteContacto: React.FC<PropsDatosGenerales> = ({ razon: razonInicia
   const [ciudad, setCiudad] = useState<string>(ciudadInicial);
   const [nombre, setNombre] = useState<string>(nombreInicial);
   const [email, setEmail] = useState<string>(emailInicial);
-  const [celular, setCelular] = useState<string>(celularInicial);
+  const [phone, setPhone] = useState<string>(''); // State for phone number
   const [telefono, setTelefono] = useState<string>(telefonoInicial);
   const [vendedor, setVendedor] = useState<string>(vendedorInicial);
 
@@ -50,7 +54,17 @@ const CardClienteContacto: React.FC<PropsDatosGenerales> = ({ razon: razonInicia
           <h1 className="text-lg font-bold p-4">Contacto</h1>
           <input className="text-large my-2 p-1 border rounded-md" type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
           <input className="text-large my-2 p-1 border rounded-md" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input className="text-large my-2 p-1 border rounded-md" type="text" placeholder="Celular" value={celular} onChange={(e) => setCelular(e.target.value)} />
+           <PhoneInput
+          defaultCountry="ua"
+          value={phone}
+          onChange={(phone) => setPhone(phone)}
+          inputProps={{
+            name: "phone",
+            required: true,
+            autoFocus: true,
+            placeholder: "Celular"
+          }}
+        /> 
           <input className="text-large my-2 p-1 border rounded-md" type="text" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
           <input className="text-large my-2 p-1 border rounded-md" type="text" placeholder="Vendedor" value={vendedor} onChange={(e) => setVendedor(e.target.value)} />
         </div>
