4b0a43ebeaeedb0ed950437f1f66b74b78b2612a
manu-lucas <lmechegaray@wecom.global>
2024-03-12 15:31:44 -0300

    add compenets nuevocliente

---
+++ src/components/Elements/Cards/CardClienteDGeneral.tsx
@@ -0,0 +1,158 @@
+import React, { useState, ChangeEvent } from "react";
+import { Card, CardHeader, Accordion, AccordionItem  } from "@nextui-org/react";
+// import { PhoneInput } from "react-international-phone";
+// import "react-international-phone/style.css";
+import Inputs from "../Inputs/Inputs";
+import TextTareas from "../Inputs/TextTareas";
+
+
+
+const CardClienteDGeneral: React.FC = () => {
+  const [inputValue, setInputValue] = useState({
+    razon: "",
+    rut: "",
+    giro: "",
+    direccion: "",
+    comuna: "",
+    ciudad: "",
+    notas: "",
+    cPago: "",
+    lcredito: "",
+    lprecios: "",
+    piepagina: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setInputValue((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <Card className=" w-9/12	 m-5 bg-slate-400">
+      <CardHeader className="w-full ">
+        <div className=" w-full ">
+          <h1 className="text-lg font-bold p-4">Datos Generales</h1>
+          <div className="flex flex-row">
+            <Inputs
+              title="Razon social o nombre"
+              type="text"
+              placeholder="Ej:Colmena G.C"
+              name="razon"
+              value={inputValue.razon}
+              onChange={handleChange}
+            />
+
+            <Inputs
+              title="Rut"
+              type="text"
+              placeholder="Ej: 4839164"
+              name="rut"
+              value={inputValue.rut}
+              onChange={handleChange}
+            />
+            <Inputs
+              title="Giro"
+              type="text"
+              placeholder=""
+              name="giro"
+              value={inputValue.giro}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-row my-7">
+            <Inputs
+              title="Dirección"
+              type="text"
+              placeholder="Ej:Av.Suecia 243"
+              name="direccion"
+              value={inputValue.direccion}
+              onChange={handleChange}
+            />
+
+            <Inputs
+              title="Comuna"
+              type="text"
+              placeholder="Ej:Provindencia"
+              name="comuna"
+              value={inputValue.comuna}
+              onChange={handleChange}
+            />
+            <Inputs
+              title="Ciudad"
+              type="text"
+              placeholder="Ej.Santiago"
+              name="ciudad"
+              value={inputValue.ciudad}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </CardHeader>
+
+      <Accordion className="m-4 font-bold">
+        <AccordionItem
+        className=" font-bold"
+          key="2"
+          aria-label="Accordion 2"
+          title={
+        <h2 className="border w-48 rounded-full	 bg-slate-50 absolute right-5 text-xs	 ">
+         Campos Avanzados
+        </h2>
+      }
+        
+        >
+          <div>
+            <div className="flex flex-row">
+              <Inputs
+                title="C.de pago"
+                type="text"
+                placeholder="-"
+                name="cPago" // Asegúrate de que Inputs acepte y pase este prop al elemento input
+                value={inputValue.cPago}
+                onChange={handleChange}
+              />
+              <Inputs
+                title="Linea de crédito"
+                type="text"
+                placeholder="Ej:40000"
+                name="lcredito"
+                value={inputValue.lcredito}
+                onChange={handleChange}
+              />
+              <Inputs
+                title="Lista de precios"
+                type="text"
+                placeholder=""
+                name="lprecios"
+                value={inputValue.lprecios}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <TextTareas
+                title="Notas"
+                placeholder=""
+                name="notas" // Asegúrate de que TextTareas acepte y pase este prop al elemento textarea
+                value={inputValue.notas}
+                onChange={handleChange}
+              />
+
+              <TextTareas
+                title="Pie de página para proyecto"
+                placeholder=""
+                name="piepagina" // Asegúrate de que TextTareas acepte y pase este prop al elemento textarea
+                value={inputValue.piepagina}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </AccordionItem>
+      </Accordion>
+    </Card>
+  );
+};
+
+export default   CardClienteDGeneral ;
--- src/components/Elements/Inputs/Inputs.tsx
+++ src/components/Elements/Inputs/Inputs.tsx
@@ -1,22 +1,27 @@
 import React, { ChangeEvent } from 'react';
 
 interface InputProps {
+  title: string;
   type: string;
   placeholder?: string;
   value: string;
+  name: string; // Añadir el prop 'name'
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Inputs: React.FC<InputProps> = ({ type, placeholder, value, onChange }) => {
+const Inputs: React.FC<InputProps> = ({ title, type, placeholder, value, onChange, name }) => {
   return (
-
-    <input 
-  className="border-solid border-2 border-gray-400 rounded-full h-9 w-auto"
-  type={type}
-  placeholder={placeholder}
-  value={value}
-  onChange={onChange}
-/>
+    <div className='mx-8'>   
+      <h3 className="font-medium">{title}</h3>
+      <input 
+        className="border-solid border-2 border-gray-400 rounded-full h-9 w-72"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        name={name} // Asegurar que se pasa el prop 'name' al elemento input
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
