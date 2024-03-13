ade0b3cadbeca6de16ed48d4e23feef21f9b46aa
manu-lucas <lmechegaray@wecom.global>
2024-03-13 12:10:26 -0300

    add new components Proyecto

--- src/pages/Commercial.tsx
+++ src/pages/Commercial.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../components/Navegation/Nav';
 import { Outlet } from 'react-router-dom'; // Importa Outlet
+import Proyectos from './nested/Proyectos';
 // import Search from '../components/Elements/Herramientas/Search';
 // import DateSelect from '../components/Elements/Herramientas/DateSelect';
 // import SearchSelect from '../components/Elements/Herramientas/SearchSelect';
@@ -14,6 +15,8 @@ const Commercial: React.FC = () => {
   return (
 <div className='h-screen w-full bg-blue-200 flex flex-row'>
       <Nav />
+
+    <Proyectos />
       {/* <Search />
       <DateSelect />
       <SearchSelect />
