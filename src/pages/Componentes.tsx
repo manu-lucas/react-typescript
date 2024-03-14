7b43d6f6f51bbe4526b33a193471638769e03401
agugomv <agugomv@gmail.com>
2024-03-14 12:44:03 -0300

    se agregan componentes en el repositorio /componentes

--- src/pages/Componentes.tsx
+++ src/pages/Componentes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from '../components/Navegation/Nav';
+import { FaAngleDown } from 'react-icons/fa';
 import Aside from '../components/Aside/Aside';
 import AsideComercial from '../components/Aside/AsideComercial';
 import Button from '../components/Elements/Button/Button';
@@ -19,9 +20,6 @@ import Inputs from '../components/Elements/Inputs/Inputs';
 import TextTareas from '../components/Elements/Inputs/TextTareas';
 import Reference from '../components/Elements/Reference';
 import Login from '../components/Login/Login';
-
-
-
 
 const Componentes: React.FC = () => {
 
@@ -93,46 +91,75 @@ const Componentes: React.FC = () => {
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
           <h3 className='text-red-600'>Date Select</h3>
           <p>import DateSelect from '../components/Elements/Herramientas/DateSelect';</p>
-          <p>Componente: <pre>&lt;DateSelect /&gt;</pre></p>
+          <p>Componente: <code>&lt;DateSelect /&gt;</code></p>
           <DateSelect />
 
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Search</h3>
+          <hr />
+          <Search />
+          <hr />
+          <p>Ruta: import Search from '../components/Elements/Herramientas/Search';</p>
+          <p>Componente: <code>&lt;Search /&gt;</code></p>
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Search Select</h3>
+          <hr />
+          <SearchSelect />
+          <hr />
+          <p>Ruta: import SearchSelect from '../components/Elements/Herramientas/SearchSelect';</p>
+          <p>Componente: <code>&lt;SearchSelect /&gt;</code></p>
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Select Period</h3>
+          <hr />
+          <SelectPeriod />
+          <hr />
+          <p>Ruta: import SelectPeriod from '../components/Elements/Herramientas/SelectPeriod';</p>
+          <p>Componente: <code>&lt;SelectPeriod /&gt;</code></p>
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Select Style</h3>
+          <hr />
+          <SelectStyle />
+          <hr />
+          <p>Ruta: import SelectStyle from '../components/Elements/Herramientas/SelectNextUI'</p>
+          <p>Componente: <code>&lt;SelectStyle /&gt;</code></p>
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Text Areas</h3>
+          <hr />
+          <TextTareas title='Area de texto' name='area de texto' value="" onChange={('')} />
+          <hr />
+          <p>Ruta: import TextTareas from '../components/Elements/Inputs/TextTareas';</p>
+          <p>Componente: <code>&lt;TextTareas title='Area de texto' name='area de texto' value="" onChange={('')} /&gt;</code></p>
         </div>
 
         <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
-          <h3 className='text-red-600'>Productividad</h3>
-          <Productividad />
+          <h3 className='text-red-600'>Reference</h3>
+          <hr />
+          <Reference title='Etiqueta de Menú' icons=<FaAngleDown /> />
+          <hr />
+          <p>Ruta: import Reference from '../components/Elements/Reference';</p>
+          <p>Componente: <code>&lt;Reference title='Etiqueta de Menú' icons= &lt;FaAngleDown/&gt; /&gt;</code></p>
         </div>
 
+        <div className='rounded-[10px] p-[10px] bg-white border solid m-[10px]'>
+          <h3 className='text-red-600'>Login</h3>
+          <hr />
+          <Login />
+          <hr />
+          <p>Ruta: import Login from '../components/Login/Login';</p>
+          <p>Componente: <code>&lt;Login /&gt;</code></p>
+        </div>
 
       </div>
-
-
-
     </div>
   );
 };
