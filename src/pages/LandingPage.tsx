8b652883e3520f12a8bef8706175f23c113abf44
lucaskulisz <lkulisz@wecom.global>
2024-03-18 16:18:14 -0300

    finish workflow

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 /* import './styles/App.scss' */
 import Login from '../components/Login/Login'
+/* import DateSelect from '../components/Navegation/DateSelect'; */
 
 
 const LandingPage: React.FC = () => {
  return (
      <div  className='h-screen w-full bg-green-200	' >
-      <Login />
+     <Login />
+     
    {/* {<DateSelect/>} */}
    </div>
    
--- src/pages/administration.tsx
+++ src/pages/administration.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Nav from '../components/Navegation/Nav';
 import Testing from './Testing';
 import SearchSelect from '../components/Elements/Herramientas/SearchSelect';
@@ -6,47 +6,39 @@ import DateSelect from '../components/Elements/Herramientas/DateSelect';
 import Search from '../components/Elements/Herramientas/Search';
 import Button from '../components/Elements/Button/Button';
 
-const Administration: React.FC = () => {
-  // State variables to store selected start and end dates
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
 
-  // Callback functions to update selected start and end dates
-  const handleStartDateChange = (date: Date | null) => {
-    setStartDate(date);
-  };
 
-  const handleEndDateChange = (date: Date | null) => {
-    setEndDate(date);
-  };
 
-  // Filtering logic to filter data based on selected start and end dates
-  // You can replace this with your actual filtering logic
-  const filteredData = []; // Implement your filtering logic here
+const Administration
+: React.FC = () => {
 
+    
   return (
-    <div className='h-screen w-full bg-green-200 flex flex-row'>
-      <Nav /> {/* Render Nav component on the left side */}
+
+<div className='h-screen w-full bg-green-200 flex flex-row'>
+  <Nav /> {/* Render Nav component on the left side */}
+  
       <div className="flex flex-col w-full"> {/* Create a column layout for the remaining content */}
         <div className='h-screen w-full bg-green-200 flex flex-row'>
-          {/* Render DateSelect component and pass callback functions */}
-          <DateSelect 
-            startDate={startDate} 
-            endDate={endDate} 
-            onStartDateChange={handleStartDateChange} 
-            onEndDateChange={handleEndDateChange} 
-          />
+        
+          <DateSelect />
+          
         </div>
         <div className='h-screen w-full bg-green-200 flex flex-row'>
           <Search />  
-          <SearchSelect /> {/* Render SearchSelect component */}
-          <Button name={'EMITIDAS'} /><Button name={'PENDIENTES'} /><Button name={'RECURRENTES'} />
+        <SearchSelect /> {/* Render SearchSelect component */}
+        <Button name={'EMITIDAS'} /><Button name={'PENDIENTES'} /><Button name={'RECURRENTES'} />
         </div>
-        {/* Pass filtered data to Testing component */}
-        <Testing data={filteredData} />
-      </div>
-    </div>
+    <Testing /> {/* Render Testing component */}
+    
+    {/* Add more components or divs here */}
+  </div>
+</div>
+            
+    
+    
   );
 };
 
-export default Administration;
+export default Administration
+;
