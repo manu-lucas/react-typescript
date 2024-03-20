7289edae1a43e47443edab65ea3de2df01a40816
lucaskulisz <lkulisz@wecom.global>
2024-03-20 13:33:28 -0300

    trasactionComponent

--- src/pages/administration.tsx
+++ src/pages/administration.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../components/Navegation/Nav";
 import TableAdministration from "../components/Elements/Tables/TableAdministration";
 import SearchSelect from "../components/Elements/Herramientas/SearchSelect";
@@ -8,11 +8,42 @@ import Button from "../components/Elements/Button/Button";
 import Transaction from "../components/Elements/Tables/Transaction";
 
 const Administration: React.FC = () => {
-  const [showTransaction, setShowTransaction] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
-  const toggleTransaction = () => {
-    setShowTransaction((prevState) => !prevState);
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
   };
+
+  const [showTransactionModal, setShowTransactionModal] = useState(false);
+
+  const toggleTransactionModal = () => {
+    setShowTransactionModal(!showTransactionModal);
+  };
+
+  const handleCloseTransactionModal = () => {
+    setShowTransactionModal(false);
+  };
+
+  useEffect(() => {
+    const handleCloseOnClickOutside = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        !e.target.closest(".modal-content")
+      ) {
+        setShowTransactionModal(false);
+      }
+    };
+
+    if (showTransactionModal) {
+      document.addEventListener("mousedown", handleCloseOnClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleCloseOnClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleCloseOnClickOutside);
+    };
+  }, [showTransactionModal]);
 
   return (
     <div className="h-screen w-full bg-green-200 flex flex-row">
@@ -21,22 +52,28 @@ const Administration: React.FC = () => {
         <div className="h-screen w-full bg-green-200 flex flex-row">
           <DateSelect />
           <div className="h-5">
-            <Button name="+Transaccion" changeState={toggleTransaction} />
+            <Button name="+Transaccion" changeState={toggleTransactionModal} />
           </div>
         </div>
         <div className="h-screen w-full bg-green-200 flex flex-row p-10">
-          <Search />
+          <Search onSearchChange={handleSearchChange} />
           <SearchSelect />
           <Button name="EMITIDAS" />
           <Button name="PENDIENTES" />
           <Button name="RECURRENTES" />
         </div>
-        <TableAdministration />
-        {showTransaction && <Transaction />}
+        <TableAdministration searchValue={searchValue} />
+        {showTransactionModal && (
+          <div className="w-full absolute h-screen right-0 top-0 bg-gray-700 bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
+            <div className="bg-white p-8 rounded-lg modal-content">
+              <Transaction />
+              <Button name="Cerrar" changeState={handleCloseTransactionModal} />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-  //aa
 };
 
 export default Administration;
