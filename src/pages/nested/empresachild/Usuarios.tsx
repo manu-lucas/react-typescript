0c8889e30c5359f3703e2db5a96c03a3d7de85f2
manu-lucas <lmechegaray@wecom.global>
2024-03-22 15:03:57 -0300

    add changes to react query

--- src/pages/nested/empresachild/Usuarios.tsx
+++ src/pages/nested/empresachild/Usuarios.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { useStore } from '../../../store/useStore';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useStore } from "../../../store/useStore";
+import { Link } from "react-router-dom";
+import {
+   useQuery
+} from '@tanstack/react-query'
+import {getProducts} from "../../../api/Request"
+
+
 
 const Usuarios: React.FC = () => {
-  const [bears,increase] = useStore((state) =>[
-    state.bears,
-    state.increase
-  ]);
+  const { bears, increase} = useStore((state) => ({
+    bears: state.bears,
+    increase: state.increase,
+    // getPost: state.getPosts,
+    // posts: state.posts,
+  }));
+
+  const {isLoading,data,isError,error}= useQuery({
+    queryKey:['products'],
+    queryFn: getProducts
+  })
+
+if(isLoading)return <div>Loading...</div>
+else if (isError) return <div>Error:{error.message}</div>
+
+  // useEffect(() => {
+  //   getPost();
+  // }, []);
+  // console.log(posts);
 
   return (
-    <div className='h-screen w-85 bg-green-200'>
+    <div className="h-screen w-85 bg-green-200">
       <h2>Usuarios</h2>
-      <h3>{bears}</h3>
-      <button   onClick={()=>increase()}>incrementar</button>
 
-      <div className='block'></div>
+      <h3>{bears}</h3>
+      <button onClick={() => increase(2)}>incrementar</button>
+
+      <div className="block"></div>
+
       <Link to="/miempresa/productos">ir a</Link>
     </div>
   );
--- src/store/useStore.tsx
+++ src/store/useStore.tsx
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 
 
+interface Post{
+  id:number
+  title:string
+  body:string
+}
+
 interface BearState {
     bears: number
-    increase: (by: number) => void
+    increase: (value: number) => void
+    posts:Post[]
+    getPosts:()=>Promise<void> 
   }
 
 
   export const useStore = create<BearState>()((set) => ({
     bears: 0,
-    increase: () => set((state) => ({ bears: state.bears + 1 })),
-}))
+    increase: (value:number) => set((state) => ({ bears: state.bears + value })),
+    posts:[],
+    
+    getPosts: async ()=>{
+      const result = await fetch("https://jsonplaceholder.typicode.com/posts")
+      const converResult = await result.json()
+      set((state) => ({ ...state, posts: converResult }));    }
+
+  }))
