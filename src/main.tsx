e64b1f350fc4bffe9d0159999b58c6851be15128
manu-lucas <lmechegaray@wecom.global>
2024-03-26 15:29:59 -0300

    add new characterts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { NextUIProvider } from "@nextui-org/react";
+import {NextUIProvider} from "@nextui-org/react";
 import { DateProvider } from './components/Contexts/DateContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
